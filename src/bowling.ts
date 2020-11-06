dd4c7e1e6b04c882ca9a282f8db9aa6c389ccc76
Tom <thomas.spencer@codurance.com>
2020-11-06 19:27:37 +0000

    Refactor to filtered

--- src/bowling.ts
+++ src/bowling.ts
@@ -1,7 +1,7 @@
 export const game = (input: string): number => {
   const filtered = input.replace("|", "");
   let result = 0;
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < filtered.length; i++) {
     if (input.charAt(i) == "/") {
       result += 10 - parseInt(filtered.charAt(i - 1));
       result += parseInt(filtered.charAt(i + 1));
